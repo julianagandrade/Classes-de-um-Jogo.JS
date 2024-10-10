e586718fef7f2fa4123b6f449998c2f121dc9369
julianagandrade <guimaraes.juliana575@gmail.com>
2024-10-10 20:39:19 -0300

    Adicionando o código

--- Index.js
+++ Index.js
@@ -1,1 +1,41 @@
-console.log("Olá!");
+class Hero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo.toLowerCase()) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'ataque desconhecido';
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}.`);
+    }
+}
+
+
+const heroi1 = new Hero('Gandalf', 3000, 'mago');
+const heroi2 = new Hero('Aragorn', 87, 'guerreiro');
+const heroi3 = new Hero('Li Mu Bai', 30, 'monge');
+const heroi4 = new Hero('Naruto', 17, 'ninja');
+
+heroi1.atacar(); 
+heroi2.atacar(); 
+heroi3.atacar(); 
+heroi4.atacar(); 
+
